4068b916219ec1000b9a69ea079e4e97166d2e3e
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-13 23:26:58 -0400

    Found the longest word in the string

---
+++ findlongestword.js
@@ -0,0 +1,8 @@
+function findLongestWordLength(str) {
+    let newWords = str.split(' ') //this will split the string into array of words separated by spaces! ['the' 'quick] vs ['t' 'h' 'e']
+    let wordLength = newWords.map(word => word.length) //map() creates an array with the length of each word
+      return Math.max(...wordLengths) //math.max goes into wordLength array and returns highest number 
+    // "..." is a spread operator, in this case it is spreading an array (wordLengths) out into a function's arguments. 
+    }
+    
+    findLongestWordLength("The quick brown fox jumped over the lazy dog");
