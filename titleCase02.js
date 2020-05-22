34bc02ef4a5e41f2325bdea0841d0eb78a7d3778
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-05-21 22:17:24 -0400

    titleCase02FINAL

--- titleCase02.js
+++ titleCase02.js
@@ -1,12 +1,10 @@
 function titleCase(str) {
     let array = str.toLowerCase().split(' ');
-    let newSentence = '';
 
     for (let i = 0; i < array.length; i++) {
-        newSentence += array[i][0].toUpperCase() + array[i].substr(1) + ' ';
+        array[i] = array[i][0].toUpperCase() + array[i].substr(1);
         }
-        str = newSentence;
+        str = array.join(' ');
         return str;
     }
-  
   console.log(titleCase("I'm a little tea pot"));
